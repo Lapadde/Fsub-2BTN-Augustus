557d705e96434e493f880ed1ea8342aa7743ed7f
Tpk <108146821+Lapadde@users.noreply.github.com>
2024-08-28 19:57:23 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ class Bot(Client):
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Hey 🖐")
+            test = await self.send_message(chat_id = db_channel.id, text = "Bot Aktif!")
             await test.delete()
         except Exception as e:
             self.LOGGER(__name__).warning(e)
