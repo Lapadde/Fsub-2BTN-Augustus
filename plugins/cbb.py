2591969d2d39bc27b087213b555e261e9f5bbde5
Tpk <108146821+Lapadde@users.noreply.github.com>
2024-08-28 19:44:51 +0800

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,38 @@
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>Join VIP ✅ :</b> <a href='https://t.me/estelerkuu'>Admin</a> \n<b>🧑‍💻 Developer :</b> <a href='https://t.me/estelerkuu'>Admin</a>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Tutup", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
+
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @Madflix_Bots
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
